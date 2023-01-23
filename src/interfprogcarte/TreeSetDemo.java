5434056f3ae8d925c78231d97b492b3c138be75a
Anca G <anca.gherman2013@gmail.com>
2023-01-23 14:02:11 +0200

    TreeSetDemo

---
+++ src/interfprogcarte/TreeSetDemo.java
@@ -0,0 +1,30 @@
+package interfprogcarte;
+import java.util.*;
+
+public class TreeSetDemo {
+    // Java program to demonstrate the
+// working of a TreeSet  - set care le tine in permanenta ordonate
+   // Main Method
+        public static void main(String args[]) {
+            // Creating TreeSet and
+            // adding elements
+            TreeSet<String> ts = new TreeSet<String>();
+
+            ts.add("Sorina");
+            ts.add("Maria");
+            ts.add("Ana");
+            ts.add("Isabela");
+            ts.add("Anca");
+            ts.add("Maria");
+            ts.add("Ana");
+
+            //printing elements with toprint:
+            System.out.println(ts);
+            // Traversing elements with iterator
+            Iterator<String> itr = ts.iterator();
+            while (itr.hasNext()) {
+                System.out.println(itr.next());
+            }
+        }
+}
+
